931425f64af3076310ac85d969fcc0be33f6bb87
Ubuntu <ubuntu@ip-10-195-9-29.ec2.internal>
2013-07-08 14:49:46 +0000

    edit to web.js - first attempt

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
 
-var app = express.createServer(express.logger());
+var fs = fsRequire('fs"');
+var buffer = new Buffer(fs.readFileSync('index.html', 'utf-8'));
+
+var bufOut = buffer.toString(buffer);
+
+Var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(bufOut);
 });
 
 var port = process.env.PORT || 5000;
