156a2f7180dd8acf5e311238bcec5e525f4a8c2c
Ubuntu <ubuntu@ip-10-195-9-29.ec2.internal>
2013-07-08 15:10:47 +0000

    edit to web.js - fourth attempt

--- web.js
+++ web.js
@@ -1,14 +1,13 @@
 var express = require('express');
 
-var fs = fsRequire('fs"');
-var buffer = fs.readFileSync('index.html', 'utf-8');
 
-var bufOut = buf.toString(buffer);
-
-Var app = express.createServer(express.logger());
+var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(bufOut);
+    var fs = require('fs');
+    var buffer = new buffer ();
+    response.send(buffer.toString('utc 8', fs.readFileSync("index.html")))
+
 });
 
 var port = process.env.PORT || 5000;
