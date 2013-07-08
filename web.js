9ca43c0d30bcdba5be5ce78f9de35056aff66b16
Ubuntu <ubuntu@ip-10-195-9-29.ec2.internal>
2013-07-08 14:56:59 +0000

    edit to web.js - second attempt

--- web.js
+++ web.js
@@ -1,7 +1,7 @@
 var express = require('express');
 
 var fs = fsRequire('fs"');
-var buffer = new Buffer(fs.readFileSync('index.html', 'utf-8'));
+var buffer = fs.readFileSync('index.html', 'utf-8');
 
 var bufOut = buffer.toString(buffer);
 
